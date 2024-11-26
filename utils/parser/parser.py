39f04847d9a69fb5e8a3f0e552e1dd32b80403b5
Alexander-W-BP <84023971+Alexander-W-BP@users.noreply.github.com>
2024-11-26 11:55:24 +0100

    initial comit

---
+++ utils/parser/parser.py
@@ -0,0 +1,252 @@
+import os
+import re
+from pathlib import Path
+import argparse
+
+
+
+
+def parse_train():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--game", type=str, required=True,
+                        help="game to train (e.g. 'Pong')")
+    parser.add_argument("-s", "--seed", type=int, required=True,
+                        help="seed")
+    parser.add_argument("-env", "--environments", type=int, required=True,
+                        help="number of envs used")
+    parser.add_argument("-r", "--reward", type=str, required=False, choices=["env", "human", "mixed"],
+                        help="reward mode, env if omitted")
+    parser.add_argument("-p", "--prune", type=str, required=False, choices=["default", "external"],
+                        help="use pruned focusfile (from default 'focusfiles' dir or external 'resources/focusfiles' dir. for custom pruning and or docker mount)")
+    parser.add_argument("-x", "--exclude_properties", action="store_true", help="exclude properties from feature vector")
+    parser.add_argument("--rgb", action="store_true", help="rgb observation space")
+    parser.add_argument("--progress", action="store_true", help="display a progress bar of the training process")
+    opts = parser.parse_args()
+
+    env_str = "ALE/" + opts.game +"-v5"
+    settings_str = ""
+    pruned_ff_name = None
+    hide_properties = False
+    noisy = os.environ["SCOBI_OBJ_EXTRACTOR"] == "Noisy_OC_Atari"
+    focus_dir = "resources/focusfiles"
+
+
+    reward_mode = 0
+    if opts.reward == "env":
+        settings_str += "_reward-env"
+    if opts.reward == "human":
+        settings_str += "_reward-human"
+        reward_mode = 1
+    if opts.reward == "mixed":
+        settings_str += "_reward-mixed"
+        reward_mode = 2
+
+    game_id = env_str.split("/")[-1].lower().split("-")[0]
+
+    #override some settings if rgb
+    rgb_exp = opts.rgb
+    if opts.rgb:
+        settings_str += "_rgb"
+    else:
+        settings_str += "_oc"
+
+    if opts.prune:
+        pruned_ff_name = f"pruned_{game_id}.yaml"
+    if opts.prune == "default":
+        settings_str += "_pruned"
+    if opts.prune == "external":
+        settings_str += "_pruned-external"
+        focus_dir = "resources/focusfiles"
+    if opts.exclude_properties:
+        settings_str += '_excludeproperties'
+        hide_properties = True
+
+    exp_name = opts.game + "_seed" + str(opts.seed) + settings_str
+    if noisy:
+        exp_name += "-noisy"
+
+    return {
+        "exp_name": exp_name,
+        "env": env_str,
+        "hide_properties": hide_properties,
+        "pruned_ff_name": pruned_ff_name,
+        "focus_dir": focus_dir,
+        "reward_mode": reward_mode,
+        "rgb_exp": opts.rgb,
+        "seed": opts.seed,
+        "environments": opts.environments,
+        "game": opts.game,
+        "rgb": opts.rgb,
+        "reward": opts.reward,
+        "progress": opts.progress
+    }
+
+
+
+def render_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--game", type=str, required=True,
+                        help="game to train (e.g. 'Pong')")
+    parser.add_argument("-s", "--seed", type=int, required=True,
+                        help="seed")
+    parser.add_argument("-r", "--reward", type=str, required=False, choices=["env", "human", "mixed"],
+                        help="reward mode, env if omitted")
+    parser.add_argument("-p", "--prune", type=str, required=False, choices=["default", "external"],
+                        help="use pruned focusfile (from default 'focusfiles' dir or external 'resources/focusfiles' dir. for custom pruning and or docker mount)")
+    parser.add_argument("-x", "--exclude_properties",  action="store_true", help="exclude properties from feature vector")
+    parser.add_argument("-n", "--version", type=str, required=False, help="specify which trained version. standard selects highest number")
+    parser.add_argument("--rgb", required= False, action="store_true", help="rgb observation space")
+    parser.add_argument("--record", required= False, action="store_true", help="wheter to record the rendered video")
+    parser.add_argument("--nb_frames", type=int, default=0, help="stop recording after nb_frames (or 1 episode if not specified)")
+    parser.add_argument("--print-reward", action="store_true", help="display the reward in the console (if not 0)")
+    parser.add_argument("--viper", nargs="?", const=True, default=False, help="evaluate the extracted viper tree instead of a checkpoint")
+    opts = parser.parse_args()
+
+    env_str = "ALE/" + opts.game +"-v5"
+    settings_str = ""
+    pruned_ff_name = None
+    hide_properties = False
+    variant = "scobots"
+
+    if opts.reward == "env":
+        settings_str += "_reward-env"
+    if opts.reward == "human":
+        settings_str += "_reward-human"
+    if opts.reward == "mixed":
+        settings_str += "_reward-mixed"
+
+    game_id = env_str.split("/")[-1].lower().split("-")[0]
+
+
+    if opts.rgb:
+        settings_str += "_rgb"
+        variant= "rgb"
+    else:
+        settings_str += "_oc"
+
+    if opts.prune:
+        pruned_ff_name = f"pruned_{game_id}.yaml"
+        variant =  "iscobots"
+    if opts.prune == "default":
+        settings_str += "_pruned"
+    if opts.prune == "external":
+        settings_str += "_pruned-external"
+    if opts.exclude_properties:
+        settings_str += '_excludeproperties'
+        hide_properties = True
+
+
+    exp_name = opts.game + "_seed" + str(opts.seed) + settings_str
+
+    return {
+        "exp_name": exp_name,
+        "env_str": env_str,
+        "hide_properties": hide_properties,
+        "pruned_ff_name": pruned_ff_name,
+        "variant": variant,
+        "version": opts.version or 0,
+        "game": opts.game,
+        "seed": opts.seed,
+        "reward": opts.reward,
+        "rgb": opts.rgb,
+        "record": opts.record,
+        "nb_frames": opts.nb_frames,
+        "print_reward": opts.print_reward,
+        "viper": opts.viper
+    }
+
+
+def parse_eval(parser):
+    parser.add_argument("-g", "--game", type=str, required=True,
+                        help="game to train (e.g. 'Pong')")
+    parser.add_argument("-s", "--seed", type=int, required=True,
+                        help="seed")
+    parser.add_argument("-t", "--times", type=int, required=True,
+                        help="number of episodes to eval")
+    parser.add_argument("-r", "--reward", type=str, required=False, choices=["env", "human", "mixed"],
+                        help="reward mode, env if omitted")
+    parser.add_argument("-p", "--prune", type=str, required=False, choices=["default", "external"],
+                        help="use pruned focusfile (from default 'focusfiles' dir or external 'resources/focusfiles' dir. for custom pruning and or docker mount)")
+    parser.add_argument("-x", "--exclude_properties", action="store_true", help="exclude properties from feature vector")
+    parser.add_argument("-n", "--version", type=str, required=False, help="specify which trained version. standard selects highest number")
+    parser.add_argument("--progress", action="store_true", help="display a progress bar of the training process")
+    parser.add_argument("--rgb", required= False, action="store_true", help="rgb observation space")
+    parser.add_argument("--viper", nargs="?", const=True, default=False, help="evaluate the extracted viper tree instead of a checkpoint")
+    opts = parser.parse_args()
+
+    env_str = "ALE/" + opts.game +"-v5"
+    settings_str = ""
+    pruned_ff_name = None
+    hide_properties = False
+    variant = "scobots"
+
+    if opts.reward == "env":
+        settings_str += "_reward-env"
+    if opts.reward == "human":
+        settings_str += "_reward-human"
+    if opts.reward == "mixed":
+        settings_str += "_reward-mixed"
+
+    game_id = env_str.split("/")[-1].lower().split("-")[0]
+
+
+    #override setting str if rgb
+    if opts.rgb:
+        settings_str += "_rgb"
+        variant= "rgb"
+    else:
+        settings_str += "_oc"
+
+    if opts.prune:
+        pruned_ff_name = f"pruned_{game_id}.yaml"
+        variant =  "iscobots"
+    if opts.prune == "default":
+        settings_str += "_pruned"
+    if opts.prune == "external":
+        settings_str += "_pruned-external"
+    if opts.exclude_properties:
+        settings_str += '_excludeproperties'
+        hide_properties = True
+        
+    exp_name = opts.game + "_seed" + str(opts. seed) + settings_str
+
+    if opts.version:
+        version = opts.version
+    else:
+        version = 0
+
+    return {
+        "exp_name": exp_name,
+        "env_str": env_str,
+        "hide_properties": hide_properties,
+        "pruned_ff_name": pruned_ff_name,
+        "times": opts.times,
+        "variant": variant,
+        "version": opts.version or 0,
+        "game": opts.game,
+        "seed": opts.seed,
+        "reward": opts.reward,
+        "progress": opts.progress,
+        "rgb": opts.rgb,
+        "viper": opts.viper
+    }
+
+
+def get_highest_version(agent): 
+    version = "-n"
+    full_path = Path(agent)
+    exp_name = full_path.name  # Extract the experiment name (e.g., 'something')
+
+    highest_version = 1
+    version_pattern = re.compile(rf"{exp_name}-n(\d+)")  # Regex to match 'exp_name-versionX'
+
+    for directory in Path("resources/checkpoints").iterdir():
+        if directory.is_dir():  # Ensure it's a directory
+            match = version_pattern.match(directory.name)
+            if match:
+                version_num = int(match.group(1))  # Extract the version number
+                highest_version = max(highest_version, version_num)
+    if highest_version > 1:
+        return version + str(highest_version)
+    else:
+        return ""
---
+++ utils/renderer.py
@@ -0,0 +1,250 @@
+import pickle
+import numpy as np
+import pygame
+import gymnasium as gym
+import time
+import os
+from scobi import Environment
+try:
+    from pygame_screen_record import ScreenRecorder
+    _screen_recorder_imported = True
+except ImportError as imp_err:
+    _screen_recorder_imported = False
+
+
+class Renderer:
+    window: pygame.Surface
+    clock: pygame.time.Clock
+    env: gym.Env
+    zoom: int = 4
+    fps: int = 20
+
+    def __init__(self, envs, model, record=False, nb_frames=0):
+        self.envs = envs
+        if hasattr(envs, 'venv') and hasattr(envs.venv, 'envs'):
+            self.env = envs.venv.envs[0]
+            self.rgb_agent = False
+        elif hasattr(envs, 'envs'):
+            self.env = envs.envs[0]  # Handles cases where envs are in a DummyVecEnv or similar
+            self.rgb_agent = True
+        else:
+            self.env = envs
+            self.rgb_agent = True
+        self.model = model
+        self.current_frame = self._get_current_frame()
+        self._init_pygame(self.current_frame)
+        self.paused = False
+
+        self.current_keys_down = set()
+        self.current_mouse_pos = None
+        if not self.rgb_agent:
+            self.keys2actions = self.env.oc_env.unwrapped.get_keys_to_action()
+
+        self.ram_grid_anchor_left = self.env_render_shape[0] + 28
+        self.ram_grid_anchor_top = 28
+
+        self.active_cell_idx = None
+        self.candidate_cell_ids = []
+        self.current_active_cell_input : str = ""
+
+        self.human_playing = False
+        self.print_reward = False
+        self._recording = False
+
+        if record:
+            if _screen_recorder_imported:
+                self._screen_recorder = ScreenRecorder(60)
+                self._screen_recorder.start_rec()
+                self._recording = True
+                self.nb_frames = nb_frames
+            else:
+                print("Screen recording not available. Please install the pygame_screen_record package.")
+                exit(1)
+        else:
+            self.nb_frames = np.inf
+
+    def _init_pygame(self, sample_image):
+        pygame.init()
+        pygame.display.set_caption("OCAtari Environment")
+        sample_image = np.repeat(np.repeat(np.swapaxes(sample_image, 0, 1), self.zoom, axis=0), self.zoom, axis=1)
+        self.env_render_shape = sample_image.shape[:2]
+        window_size = self.env_render_shape[:2]
+        self.window = pygame.display.set_mode(window_size)
+        self.clock = pygame.time.Clock()      
+
+    """ def run(self):
+        self.running = True
+        obs = self.envs.reset()
+        i = 1
+        while self.running:
+            self._handle_user_input()
+            if not self.paused:
+                if self.human_playing:
+                    action = [self._get_action()]
+                    time.sleep(0.05)
+                else:
+                    action, _ = self.model.predict(obs, deterministic=True)
+                obs, rew, done, infos = self.envs.step(action)
+                self.env.sco_obs = obs
+                self.current_frame = self._get_current_frame()
+                if self.print_reward and rew[0]:
+                    print(rew[0])
+                if done:
+                    if self._recording and self.nb_frames == 0:
+                        self._save_recording()
+                    obs = self.envs.reset()
+                elif self._recording and i == self.nb_frames:
+                    self._save_recording()
+            self._render()
+
+            if self.rgb_agent:
+                self.clock.tick(self.fps)
+        pygame.quit() """
+    def run(self):
+        self.running = True
+        obs = self.envs.reset()  # Umgebung wird zurÃ¼ckgesetzt
+        i = 1
+        env = Environment(env_name="ALE/Pong-v5")
+        
+
+        while self.running:
+            import ipdb; ipdb.set_trace()
+            self._handle_user_input()
+            if not self.paused:
+                if self.human_playing:
+                    action = [self._get_action()]
+                    time.sleep(0.05)
+                else:
+                    # Aufruf der neuen Funktion vor der Aktion
+                    print("Vector Entry Descriptions:")
+                    vector_descriptions = env.get_vector_entry_descriptions()
+                    #print(vector_descriptions)
+                    for a, b in zip(vector_descriptions, obs[0]):
+                        print(a, b)
+
+                    # Modell sagt Aktion vorher
+                    action, _ = self.model.predict(obs, deterministic=True)
+
+                    print("------------>>>> Action ------------>>>>: ", action)  # Ausgabe der Aktion auf der Kommandozeile
+
+                    # Entscheidung umsetzen
+                    obs, rew, done, infos = self.envs.step(action)
+                    self.env.sco_obs = obs
+                    self.current_frame = self._get_current_frame()
+
+                    if self.print_reward and rew[0]:
+                        print(rew[0])
+
+                    if done:
+                        if self._recording and self.nb_frames == 0:
+                            self._save_recording()
+                        obs = self.envs.reset()
+                    elif self._recording and i == self.nb_frames:
+                        self._save_recording()
+
+                self._render()
+
+                if self.rgb_agent:
+                    self.clock.tick(self.fps)
+        pygame.quit()
+
+
+
+
+
+    def _get_action(self):
+        pressed_keys = list(self.current_keys_down)
+        pressed_keys.sort()
+        pressed_keys = tuple(pressed_keys)
+        if pressed_keys in self.keys2actions.keys():
+            return self.keys2actions[pressed_keys]
+        else:
+            return 0  # NOOP
+
+    def _save_recording(self):
+        self._screen_recorder.stop_rec()	# stop recording
+        filename = f"{self.env.oc_env.game_name}.avi"
+        i = 0
+        while os.path.exists(filename):
+            i += 1
+            filename = f"{self.env.oc_env.game_name}_{i}.avi"
+        self._screen_recorder.save_recording(filename)
+        print(f"Recording saved as {filename}")
+        self._recording = False
+
+    def _handle_user_input(self):
+        self.current_mouse_pos = np.asarray(pygame.mouse.get_pos())
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:  # window close button clicked
+                if self._recording:
+                    self._save_recording()
+                self.running = False
+
+            elif event.type == pygame.KEYDOWN:  # keyboard key pressed
+                if event.key == pygame.K_p:  # 'P': pause/resume
+                    self.paused = not self.paused
+
+                elif event.key == pygame.K_r:  # 'R': reset
+                    self.env.reset()
+                
+                elif event.key == pygame.K_h:  # 'H': toggle human playing
+                    self.human_playing = not self.human_playing
+                    if self.human_playing:
+                        print("Human playing")
+                    else:
+                        print("AI playing")
+                
+                elif event.key == pygame.K_m:  # 'M': save snapshot
+                    snapshot = self.env._ale.cloneState()
+                    pickle.dump(snapshot, open("snapshot.pkl", "wb"))
+                    print("Saved snapshot.pkl")
+
+                elif event.key == pygame.K_ESCAPE and self.active_cell_idx is not None:
+                    self._unselect_active_cell()
+
+                elif (event.key,) in self.keys2actions.keys():  # env action
+                    self.current_keys_down.add(event.key)
+
+                elif pygame.K_0 <= event.key <= pygame.K_9:  # enter digit
+                    char = str(event.key - pygame.K_0)
+                    if self.active_cell_idx is not None:
+                        self.current_active_cell_input += char
+
+                elif event.key == pygame.K_BACKSPACE:  # remove character
+                    if self.active_cell_idx is not None:
+                        self.current_active_cell_input = self.current_active_cell_input[:-1]
+
+                elif event.key == pygame.K_RETURN:
+                    if self.active_cell_idx is not None:
+                        if len(self.current_active_cell_input) > 0:
+                            new_cell_value = int(self.current_active_cell_input)
+                            if new_cell_value < 256:
+                                self._set_ram_value_at(self.active_cell_idx, new_cell_value)
+                        self._unselect_active_cell()
+
+            elif event.type == pygame.KEYUP:  # keyboard key released
+                if (event.key,) in self.keys2actions.keys():
+                    self.current_keys_down.remove(event.key)
+
+    def _get_current_frame(self):
+        if self.rgb_agent:
+            return self.env.render()
+        else:
+            return self.env._obj_obs
+
+    def _render(self, frame = None):
+        self.window.fill((0,0,0))  # clear the entire window
+        self._render_atari(frame)
+        pygame.display.flip()
+        pygame.event.pump()
+
+    def _render_atari(self, frame = None):
+        if frame is None:
+            frame = self.current_frame
+        frame = np.swapaxes(np.repeat(np.repeat(frame, self.zoom, axis=0), self.zoom, axis=1), 0, 1)
+        frame_surface = pygame.Surface(self.env_render_shape)
+        pygame.pixelcopy.array_to_surface(frame_surface, frame)
+        self.window.blit(frame_surface, (0, 0))
+        self.clock.tick(60)
